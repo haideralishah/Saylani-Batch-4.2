1bcb2c239d4d0272dd385f541e696012191e47ed
Haider <headeralishah@gmail.com>
2017-04-24 11:59:52 +0500

    Lab and theory work till 23rd April, 2017

---
+++ Class Code/Javascript/Chapter 2 - 10/app.js
@@ -0,0 +1,70 @@
+// var firstNumber = 5;
+// // // firstNumber = firstNumber + 1;
+
+// // firstNumber += 1;
+
+// // // console.log(firstNumber);
+
+
+
+// console.log(firstNumber++);
+// console.log(firstNumber);
+// console.log(firstNumber);
+
+// var num = 5;
+// //          5     + 6  -   5   +  5     + 4 
+// var total = num++ + num - --num + num-- + num;
+// console.log(total);
+
+
+
+
+// var num1 = 2;
+// var num2 = 3;
+// //          3      - 2      +  3
+// var total = ++num1 - --num2 + num1;
+// console.log(total);
+
+
+
+
+// var num1 = 7;
+// var num2 = 2;
+
+// var total = num1 + num2 + (++num1 - --num2);
+// console.log(total);
+
+
+
+
+
+// var num1 = '1';
+// var num2 = 1;
+// var total = 'Haider' + ' ' + 'Ali';
+// console.log(total);
+
+
+// var firstName = '9';
+// var lastName = '3';
+// var fullName = firstName / lastName;
+// console.log(typeof fullName);
+
+
+// window. alert ( " Hello World " );
+
+
+
+
+
+// var userName = prompt('What is your name?', "I don't have a name I am a robot");
+
+
+// alert(userName);
+
+
+
+
+
+var num1 = +prompt('write your lucky number'); // '5'
+var num2 = +prompt('write your unlucky number'); // '8'
+console.log(num1 + num2);
---
+++ Class Code/Javascript/Introduction/app.js
@@ -0,0 +1,80 @@
+// console.log(haider);
+
+
+// var userName = 'Hello World!';
+// console.log(userName);
+
+// userName = 'Haider';
+
+// console.log(userName);
+
+// var maleStudent = 35;
+// var femaleStudent = 35;
+// var totalStudents = maleStudent / femaleStudent;
+// console.log(totalStudents);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var month = prompt('month');
+// if (month.length > 3) {
+//     month = month.slice(0, 3);
+// }
+// console.log(month);
+
+
+
+
+
+
+
+
+
+
+
+// var str = prompt('any string');
+
+// for (var i = 0; i < str.length; i++) {
+//     console.log(str[i] + str[i + 1]);
+//     if (str.slice(i, i + 2) == '  ') {
+
+//         alert('no double spaces');
+//     }
+
+// }
+
+
+
+
+// var text = 'a quick Haider voluptatem fox jumps over the lazy dog';
+
+// var copy = text.slice(17, 17 + 12)
+
+
+// for (var i = 0; i < text.length; i++) {
+//     // console.log(text.slice(i, i + 12));
+//     if (text.slice(i, i + 6) === "Haider") {
+//         // console.log(text.slice(i, i + 12));
+//         // console.log(i);
+//         text = text.slice(0, i) + "Ali" + text.slice(i + 6);
+//     }
+// }
+// console.log(text);
+
+
+
+
+var text = 'a quick Haider voluptatem fox jumps over the lazy dog';
+
+
+var idx = text.indexOf('Haider');
