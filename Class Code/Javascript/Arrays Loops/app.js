f78ab930ca005008a131bbba10a1949548a54b19
Haider <headeralishah@gmail.com>
2017-05-07 19:31:48 +0500

    loop and array done.

---
+++ Class Code/Javascript/Arrays Loops/app.js
@@ -0,0 +1,98 @@
+// var city01 = 'Karachi';
+// var city02 = 'Lahore';
+// var city03 = 'Islamabad';
+
+// var cities = [];
+// console.log(cities);
+// cities[0] = 'Karachi';
+// cities[1] = 'Lahore';
+// cities['username'] = 'Haider Ali';
+// console.log(cities);
+
+
+// var citiesOfDiffCount = [['Karachi', 'Lahore', 'Islamabad'], ['Mumbai', 'Dehli', 'Kolkatta'], ['NY', 'LY', 'Washington']];
+// console.log(citiesOfDiffCount[0][1]);
+
+
+
+
+
+// var fruits = ['Apple', 'Grapes', 'Banana', 'Mango'];
+// fruits.pop(); // remove last index
+// console.log(fruits);
+
+// fruits['specialfruits'] = 'peach';
+// fruits.push('Peach'); // add an element at the end of an arrey
+// console.log(fruits);
+
+// var fruits = ['Apple', 'Grapes', 'Banana', 'Mango', 'Banana', 'Banana', 'Banana'];
+// var idx = fruits.indexOf('watermelon');
+//  console.log(idx);
+// fruits.splice(idx, 1, 'peach');
+
+
+
+// for (var i = 100; i >= 0; i--) {
+//     if (i % 5 === 0) {
+//         document.write('This is divisible by 5' + '<br>');
+//     }
+//     else {
+//         document.write(i + '<br>');
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var userName = ['Ali', 'Maaz', 'Tariq'];
+// var userInput = prompt('what is your name?');
+// if (userName.indexOf(userInput) === -1) {
+//     console.log('user does not exist');
+// }
+// else {
+//     console.log('Welcome ' + userInput);
+// }
+
+
+
+
+
+
+
+
+// var userName = ['Ali', 'Maaz', 'Tariq'];
+// var userInput = prompt('what is your name?');
+// var exist = false;
+// for (var i = 0; i < userName.length; i++) {
+//     if (userInput === userName[i]) {
+//         exist = true;
+//     }
+// }
+// if (exist === true) {
+//     console.log('Welcome ' + userInput);
+// }
+// else {
+//     console.log('user does not exist');
+// }
+
+
+
+var firstName = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+for (var i = 0; i < 36; i = i + 2) {
+    // for (var j = 0; j < lastNames.length; j++) {
+    //     console.log(firstName[i] + ' ' + lastNames[j]);
+    // }
+
+    console.log('hello');
+
+}
