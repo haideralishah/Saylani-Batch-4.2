07089aa036ec019ad9d09a19b6a27e4e830d5ea4
Haider <headeralishah@gmail.com>
2017-06-04 10:58:46 +0500

    Class code till 4th June, 2017 - chapter 26 and 27

---
+++ Class Code/Chapter 26 and 27/app.js
@@ -0,0 +1,32 @@
+
+
+// var userInput = +prompt('tell a number');
+
+// // userInput = Math.round(userInput);
+
+// //userInput = Math.ceil(userInput); // greater value
+
+
+// userInput = Math.floor(userInput); // lower value
+
+// document.getElementById('myNumber').innerHTML = userInput;
+
+
+
+
+
+
+
+var randomNumber1 = Math.random();
+
+
+
+
+
+randomNumber = randomNumber * 6;
+
+randomNumber = Math.ceil(randomNumber);
+
+document.getElementById('myNumber').innerHTML = randomNumber;
+
+
